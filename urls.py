7332974f87761278c09647de7fb8ea217725bf13
Andy Kish <agkish@gmail.com>
2009-04-08 11:13:29 -0400

    tweaked settings to handle trailing slash on URLs and i18n appropriately

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     ('^$', 'url.views.create'),
-    ('^(?P<sha_hash>[a-z\d]{1,40})/$', 'url.views.access'),
+    ('^(?P<sha_hash>[a-z\d]{1,40})$', 'url.views.access'),
 )
